026d80b24dda48ce6d3023b0c3207805fc9cc682
Kemal Abdulrahman <kemalabdulrahman6@gmail.com>
2021-11-30 16:38:18 -0800

    Initial push for Complex

---
+++ Complex/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  
+//
+//  Created by Kemal Abdulrahman on 4/14/20.
+//  Copyright © 2020 Kemal Abdulrahman. All rights reserved.
+//
+
+#include <iostream>
+
+#include<iostream>
+using namespace std;
+struct Complex{
+   int a1,b1;
+   int 
+}
+int main(){
+   int a1, b1, a2, b2, a3, b3;
+   cout<<"Enter the first complex number : "<<endl;
+   cin>> a1 >> b1;
+
+   cout<<"\nEnter second complex number : "<<endl;
+   cin>> a2 >> b2;
+   a3 = a1 * b2 - a1 * b2;
+   b3 = a1 * a2 + b1 * b2;
+   cout<<"The value after multiplication is: "<<x3<<" + "<<y3<<" i ";
+   return 0;
+}
